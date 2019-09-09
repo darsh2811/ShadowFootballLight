91d7be1b915efbd5902f9906b49c438acd44e61d
darsh2811 <darsh2019@gmail.com>
2019-09-09 15:47:39 +0530

    Basic Project Setup

---
+++ Assets/Script/LightManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+   public bool isActive;
+
+    public GameObject[] totalLights;
+    // Start is called before the first frame update
+    void Start()
+    {
+       this.isActive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TurnOnOfLight(int i)
+    {
+        //LightsOn();
+         //OnOff.instance.isActive = !OnOff.instance.isActive;
+         //this.isActive = !this.isActive;
+        totalLights[i].GetComponent<OnOff>().isActive = !totalLights[i].GetComponent<OnOff>().isActive;
+        totalLights[i].SetActive(totalLights[i].GetComponent<OnOff>().isActive);
+        
+    }
+
+    void LightsOn()
+    {
+        for(int i=0;i<totalLights.Length;i++)
+        {
+            totalLights[i].SetActive(true);
+        }
+    }
+}
